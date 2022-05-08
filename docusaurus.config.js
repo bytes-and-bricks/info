4863421498989c661a6381d45476164d6549e3e5
Sergicp77 <46504174+Sergicp77@users.noreply.github.com>
2022-05-08 18:26:33 +0200

    config updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,14 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.svg',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Bytes'n Bricks", // Usually your GitHub org/user name.
-  projectName: "Info", // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  // Internalization
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -34,17 +27,13 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bytes-and-bricks/bytes-and-bricks-site/tree/master/docs',
+            'https://github.com/bytes-and-bricks/bytes-and-bricks-site/tree/master',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bytes-and-bricks/bytes-and-bricks-site/tree/master/docs',
+            'https://github.com/bytes-and-bricks/bytes-and-bricks-site/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -78,7 +67,7 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
+        style: 'light',
         links: [
           {
             title: 'Docs',
@@ -120,7 +109,6 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Bytes'n Bricks`,
       },
       prism: {
         theme: lightCodeTheme,
